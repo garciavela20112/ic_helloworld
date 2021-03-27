f489b2e80c3b84c88852ad91af0c0d920f22a57f
Norberto Mateos <nm920@ic.ac.uk>
2021-03-27 18:15:38 +0000

    JSON Working

--- src/DBConnection/MongoConnection.java
+++ src/DBConnection/MongoConnection.java
@@ -3,35 +3,29 @@ package DBConnection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
 
 public class MongoConnection {
 
   public static void main(String[] args) throws IOException, ParseException {
 
     JSONParser parser = new JSONParser();
-    JSONArray a = (JSONArray) parser.parse(new FileReader("../../db_config.json"));
+    JSONObject config = (JSONObject) parser.parse(new FileReader("db_config.json"));
+    String username = (String) config.get("username");
+    String password = (String) config.get("password");
+    String dbname = (String) config.get("db-name");
+    String cname = (String) config.get("user-collection-name");
 
-    Object o = a.get(0);
-    JSONObject config = (JSONObject) o;
-    Object u = config.get("authentication");
-    JSONObject uname = (JSONObject) u;
-    String username = (String) uname.get("username");
-    String password = (String) uname.get("password");
 
     MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://" + username + ":"  + password +
         "@cluster0.1rt3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority)"));
-    MongoCollection<Document> database =  mongoClient.getDatabase("elgemo").getCollection("users");
+    MongoCollection<Document> database =  mongoClient.getDatabase(dbname).getCollection(cname);
     System.out.println(database.find().toString());
   }
 
