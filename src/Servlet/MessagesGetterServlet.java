1b0deace1b07991f09717b0e4a566449693eecf0
og519 <og519@ic.ac.uk>
2021-03-28 04:48:47 +0200

    MessageGetterServlet Progress

--- src/Servlet/MessagesGetterServlet.java
+++ src/Servlet/MessagesGetterServlet.java
@@ -1,4 +1,18 @@
 package Servlet;
 
+import DB.MongoConnection;
+
+@WebServlet("/messagesGetterServlet")
 public class MessagesGetterServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            MongoCollection<Document> messages = MongoConnection.DBConnect().getCollection("messages");
+            String sender = request.getParameter("sender");
+            String recipient = request.getParameter("recipient");
+            FindIterable<Document> itr = messages.find(Filters.or(Filters.and(Filter.eq("sender", sender),Filter.eq("recipient", recipient)),Filter.and(Filter.eq("sender", recipient),Filter.eq("recipient", sender))));
+            Iterator iterator = itr.iterator();
+        }
+    }
 }
+
