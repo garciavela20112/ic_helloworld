13be7703d3eb5e0fb34c65d2ae21eab7dcca38aa
og519 <og519@ic.ac.uk>
2021-03-28 04:13:42 +0200

    MessageAdderServlet without Imports

--- src/Servlet/MessageAdderServlet.java
+++ src/Servlet/MessageAdderServlet.java
@@ -1,4 +1,28 @@
 package Servlet;
 
-public class MessageAdderServlet {
+
+import java.io.IOException; 
+
+import javax.servlet.ServletException;
+
+@WebServlet("/messageAdderSevlet")
+public class MessageAdderServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        try {
+            MongoCollection<Document> messages = MongoConnection.DBConnect().getCollection("messages");
+
+            //Message Information
+            String sender = request.getParameter("sender");
+            String recipient = request.getParameter("recipient");
+            String content = request.getParameter("content");
+
+            Message msg = new Message(sender, recipient, content);
+            msg.addMessage(messages); 
+            
+        } catch (ParseException e) {
+            e.printStackTrace(); 
+        }
+    }
 }
